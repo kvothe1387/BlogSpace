15895533598252eb811b1fd4eb4a283c17574429
David McCullough <dmccullough488@gmail.com>
2025-08-27 23:06:46 -0500

    Display the fetched blogs on the page

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ fetch('https://apis.scrimba.com/jsonplaceholder/posts')
   .then(response => response.json())
   .then(data => {
     // limit to first 5 items
-    const firstFivePosts = data.slice(0, 5)
-
-    console.log(firstFivePosts)
+    const postArr = data.slice(0, 5)
+    let html = ""
+    for (let post of postArr) {
+      html += `
+        <h3>${post.title}</h3>
+        <p>${post.body}</p>
+        <hr />
+      `
+    }
+    document.getElementById('blog-list').innerHTML = html
   })
