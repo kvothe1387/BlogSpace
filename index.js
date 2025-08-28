7af449181f8d83ec65a604925905911b8545c4a4
David McCullough <dmccullough488@gmail.com>
2025-08-27 23:48:38 -0500

    Added a fixed navbar, google font 'Inter', gradient background, hover animation and initial css styling

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 fetch('https://apis.scrimba.com/jsonplaceholder/posts')
   .then(response => response.json())
   .then(data => {
+
     // limit to first 5 items
     const postArr = data.slice(0, 5)
     let html = ""
-    for (let post of postArr) {
+
+    for (let i = 0; i < postArr.length; i++) {
+      const post = postArr[i]
       html += `
-        <h3>${post.title}</h3>
-        <p>${post.body}</p>
-        <hr />
+        <article class="post">
+          <span class="post-number">Post #${i + 1}</span>
+          <h3>${post.title}</h3>
+          <p>${post.body}</p>
+        </article>
       `
     }
     document.getElementById('blog-list').innerHTML = html
