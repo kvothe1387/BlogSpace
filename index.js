64acfad5e8def504b98b793e2a3704514a6b6bcc
David McCullough <dmccullough488@gmail.com>
2025-08-28 23:48:37 -0500

    added form submit event listener

--- index.js
+++ index.js
@@ -18,3 +18,25 @@ fetch('https://apis.scrimba.com/jsonplaceholder/posts')
     }
     document.getElementById('blog-list').innerHTML = html
   })
+
+// Get the form element
+const form = document.querySelector('form')
+
+// Listen for submit event on the form
+form.addEventListener('submit', function (e) {
+  // prevent the form from refreshing the page
+  e.preventDefault()
+
+  // get the values from the form inputs
+  const postTitle = document.getElementById('post-title').value
+  const postBody = document.getElementById('post-body').value
+
+  // create an object with title & body properties
+  const postObject = {
+    title: postTitle,
+    body: postBody
+  };
+
+  // log the object to console
+  console.log(postObject)
+})
