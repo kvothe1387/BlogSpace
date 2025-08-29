fc8d33ad5b18aeac452a5a16dc013b526daf52b8
David McCullough <dmccullough488@gmail.com>
2025-08-29 17:43:19 -0500

    completed fetch req, send new post to server, and then adding the new post to lthe top of ist posts

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 fetch('https://apis.scrimba.com/jsonplaceholder/posts')
   .then(response => response.json())
   .then(data => {
-
     // limit to first 5 items
     const postArr = data.slice(0, 5)
     let html = ""
@@ -19,24 +18,40 @@ fetch('https://apis.scrimba.com/jsonplaceholder/posts')
     document.getElementById('blog-list').innerHTML = html
   })
 
-// Get the form element
-const form = document.querySelector('form')
-
-// Listen for submit event on the form
-form.addEventListener('submit', function (e) {
-  // prevent the form from refreshing the page
+document.getElementById("new-post").addEventListener("submit", function (e) {
   e.preventDefault()
-
   // get the values from the form inputs
   const postTitle = document.getElementById('post-title').value
   const postBody = document.getElementById('post-body').value
-
   // create an object with title & body properties
-  const postObject = {
+  const newPost = {
     title: postTitle,
     body: postBody
-  };
+  }
+  const options = {
+    method: "POST",
+    body: JSON.stringify(newPost),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }
 
-  // log the object to console
-  console.log(postObject)
+  fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+    .then(respone => respone.json())
+    .then(data => {
+      // Insert new post at the top of the blog list
+      document.getElementById('blog-list').insertAdjacentHTML("afterbegin", `
+        <article class="post">
+          <span class="post-number">New Post</span>
+          <h3>${data.title}</h3>
+          <p>${data.body}</p>
+        </article>
+      `)
+    })
+
 })
+
+
+
+
+
